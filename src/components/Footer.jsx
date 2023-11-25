051390d4fe7e2d9fc8b45c0c9b9ba7c234f6b07e
cabrera-carlos <cabrera.crcc@gmail.com>
2023-11-25 00:52:35 -0500

    Adding main sections and placeholder text

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer-text text-center">
+        Copyright &copy; {new Date().getFullYear()} Caribe Tax Solutions.
+        Derechos reservados.
+      </p>
+    </footer>
+  );
+}
